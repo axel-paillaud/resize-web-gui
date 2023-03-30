2e6e54b809658d72b67d72bf29227daffe3ff764
Axel Paillaud <a.paillaud75@gmail.com>
2023-03-30 16:23:33 +0200

    Front end JS, HTML, CSS OK, work with PHP API

--- src/api.php
+++ src/api.php
@@ -1,14 +1,14 @@
 <?php
 
 $receive = json_decode(file_get_contents('php://input'), true);
-$send = json_encode("hello");
+$send = json_encode($receive);
 
 header("Content-type: application/json");
 echo $send;
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+/* if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $send = json_encode("hello");
 
     header("Content-type: application/json");
     echo $send;
-}
+} */
