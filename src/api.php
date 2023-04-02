fdb106538c142173955c0766b594e2816a059d16
Axel Paillaud <a.paillaud75@gmail.com>
2023-04-02 18:50:19 +0200

    Create folder, resize and convert image with PHP API OK

--- src/api.php
+++ src/api.php
@@ -10,24 +10,24 @@ else {
 }
 
 $files = $_FILES["image"];
+$formats = $_POST["format"];
+$sizes = $_POST["size"];
+$quality = $_POST["quality"];
 
-function resizeImg($image, int $size, string $fileName, string $dirName)
+function resizeImg($image, int $size, string $filename)
 {
     $cloneImage = $image->clone();
     $cloneImage->resizeImage($size, 0, imagick::FILTER_LANCZOS, 0.5);
-    print_log("Resize $fileName to $size\n");
-    $cloneImage->writeImage("$dirName/$fileName");
-    print_log("Write $fileName in $dirName\n");
+    print_log("Resize $filename to $size\n");
     return $cloneImage;
 }
 
-function convertImg($image, int $size, string $format, string $fileName) 
+function convertImg($image, string $quality, string $format, string $fileName) 
 {
-    global $dirName;
-    $image->setOption('quality', '80');
+    $image->setOption('quality', $quality);
     $image->setImageFormat($format);
-    $image->writeImage("./$dirName/avif/$fileName-$size.avif");
-    echo "Write $fileName-$size.avif in ./$dirName/avif/\n";
+    print_log("Convert $fileName to $format\n");
+    return $image;
 }
 
 function createDir(string $dirName, string $path) 
@@ -46,6 +46,7 @@ if (!extension_loaded('imagick'))
 
 createDir("resize_images", "./");
 
+/* For each images, do */
 for ($i = 0; $i < count($files["name"]); $i++)
 {
 /*     print_log($files["name"][$i]);
@@ -62,8 +63,24 @@ for ($i = 0; $i < count($files["name"]); $i++)
     else
     {
         $image = new Imagick($files["tmp_name"]);
-        resizeImg($image, 1920, "output", "./resize_images");
+
+        /* for each format, do */
+        for ($i = 0; $i < count($formats); $i++)
+        {
+            createDir($formats[$i], "./resize_images/");
+            $formatFolder = "./resize_images/$formats[$i]/";
+
+            /* for each size, do */
+            for ($j = 0; $j < count($sizes); $j++)
+            {
+                $resizedImg = resizeImg($image, $sizes[$j], $filename);
+                $convertedImg = convertImg($resizedImg, $quality, $formats[$i], $filename);
+                $convertedImg->writeImage("$formatFolder $filename-$sizes[$j].$formats[$i]");
+            }
+        }
     }
+
+    $image->destroy();
 }
 
 /* if ($_SERVER["REQUEST_METHOD"] === "POST") {
