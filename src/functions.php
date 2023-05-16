d5fb1df430010f655a5ff0d06c2c8d51b6853488
Axel Paillaud <a.paillaud75@gmail.com>
2023-05-16 19:38:27 +0200

    remove date from print_log functions, sort  wip

--- src/functions.php
+++ src/functions.php
@@ -6,8 +6,6 @@ function print_log($string)
 {
     $folder =  "../log/error.log";
     $string = print_r($string, true);
-    $date = date('l jS \of F Y h:i:s A');
-    $string = $date . "\n" . $string;
     error_log($string . "\n\n", 3, $folder);
 }
 
